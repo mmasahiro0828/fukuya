94cc6353e1ad308fc6d7a2acdf6b70de2b4fb7f8
mmasahiro006 <masahiromasahiro06@yahoo.co.jp>
2020-01-09 20:08:46 +0900

    order確定時、サイトインジェクションで自由に金額を入れれたため、できないように修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,13 +9,7 @@ class OrdersController < ApplicationController
     def new_for_payment
         cart = current_cart
         
-
         @order = Order.new(
-            user_id: cart.user_id,
-            total_item_price: cart.cart_items.sum(:price_w_tax),
-            discount: 0,
-            fee: 0,
-            final_price: cart.cart_items.sum(:price_w_tax),
             method_of_payment: "yet",
             user_name: "#{order_params_1[:family_name]} #{order_params_1[:first_name]}",
             user_email: order_params_1[:email],
@@ -24,7 +18,6 @@ class OrdersController < ApplicationController
             shipping_adress: order_params_1[:adress],
             shipping_cond_n_room: order_params_1[:cond_n_room],
             user_phonenumber: order_params_1[:phonenumber],
-            shipping_date: "yet"
         )
 
         unless @order.valid?
@@ -41,29 +34,48 @@ class OrdersController < ApplicationController
         @order = Order.new(order_params_2)
         @date_selection = date_selection
         @token ||= params['payjp-token']
+        cart = current_cart
+        @cart_items = cart.cart_items
     
-        
-
         unless @order.valid?
             render :new_for_payment
         end
 
+
         if @order.method_of_payment == "代引き"
             @order.fee = 380
+        else
+            @order.fee = 0
         end
 
         /discount処理/
+        @order.discount = 0
+        //
 
+        @order.total_item_price = cart.cart_items.sum(:price_w_tax)
         @order.final_price = @order.total_item_price + @order.fee - @order.discount
-
         @order.shipping_date = "日: #{order_params_3[:shipping_date_1]} / 時間: #{order_params_3[:shipping_date_2]}"
-
-        @cart_items = current_cart.cart_items
+        
     end
     
 
     def create
-        order = Order.new(order_params_4)
+        order = Order.new(order_params_2)
+        cart = current_cart
+        order.user_id = cart.user_id
+        order.total_item_price = cart.cart_items.sum(:price_w_tax)
+
+        if order.method_of_payment == "代引き"
+            order.fee = 380
+        else
+            order.fee = 0
+        end
+
+        /discount処理/
+        order.discount = 0
+        //
+
+        order.final_price = order.total_item_price + order.fee - order.discount
         order.save
 
         if order.method_of_payment == "クレジットカード"
@@ -91,7 +103,7 @@ class OrdersController < ApplicationController
             cart_item.destroy
         end
 
-        current_cart.destroy
+        cart.destroy
         session[:cart_id] = nil
 
         redirect_to orders_thank_you_path(id: order.id)
@@ -122,19 +134,15 @@ class OrdersController < ApplicationController
     def order_params_2
         params.require(:order).permit(
             :method_of_payment,
-            :id,
-            :user_id,
-            :total_item_price,
-            :discount,
-            :fee,
-            :final_price,
             :user_name,
             :user_email,
             :user_email_confirmation,
             :shipping_postal_code,
             :shipping_adress,
             :shipping_cond_n_room,
-            :user_phonenumber
+            :user_phonenumber,
+            :method_of_payment,
+            :shipping_date
         )
     end
 
@@ -147,26 +155,6 @@ class OrdersController < ApplicationController
         )
     end
 
-
-    def order_params_4
-        params.require(:order).permit(
-            :id,
-            :user_id,
-            :total_item_price,
-            :discount,
-            :fee,
-            :final_price,
-            :user_name,
-            :user_email,
-            :user_email_confirmation,
-            :shipping_postal_code,
-            :shipping_adress,
-            :shipping_cond_n_room,
-            :user_phonenumber,
-            :method_of_payment,
-            :shipping_date
-        )
-    end
 
 
     def date_selection
